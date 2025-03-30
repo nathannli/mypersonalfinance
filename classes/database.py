465149c59d4b33cbaf3e1e2f04b38508d66f5b3d
Nathan Li <nathan@appscience.inc>
2025-03-29 22:03:54 -0400

    (database): refactor to use abstraction

---
+++ classes/database.py
@@ -0,0 +1,61 @@
+from abc import ABC
+from dotenv import load_dotenv
+import os
+
+import psycopg
+
+
+class PostgresDB(ABC):
+    """
+    Abstract base class for Postgres databases.
+    """
+    schema_name: str
+    debug: bool
+
+    def __init__(self, schema_name: str, debug: bool = False):
+        """
+        Initialize the PostgresDB instance.
+        """
+        load_dotenv()
+        self.uri = os.getenv("POSTGRES_CONNECTION_STRING")
+        self.schema_name = schema_name
+        self.debug = debug
+        if self.debug:
+            print(f"{self.uri=}")
+            print(f"{self.schema_name=}")
+
+    def connect(self) -> psycopg.Connection:
+        """
+        Connect to the database.
+        """
+        return psycopg.connect(self.uri)
+
+    def close(self) -> None:
+        """
+        Close the database connection.
+        """
+        self.conn.close()
+
+    def insert(self, query: str, args: tuple) -> None:
+        """
+        Insert data into the database.
+        """
+        if self.debug:
+            print(f"{query=}")
+            print(f"{args=}")
+        with self.connect() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, args)
+                conn.commit()
+
+    def select(self, query: str, args: tuple = None) -> list[tuple]:
+        """
+        Select data from the database.
+        """
+        if self.debug:
+            print(f"{query=}")
+            print(f"{args=}")
+        with self.connect() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, args)
+                return cur.fetchall()
