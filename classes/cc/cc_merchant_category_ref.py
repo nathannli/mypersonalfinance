59a783b6c70a68af6f867e4a87f74d7c3d851f3f
Nathan Li <nathan.li.1995@gmail.com>
2025-04-14 09:36:54 -0400

    (add): cc_merchant_category_ref

---
+++ classes/cc/cc_merchant_category_ref.py
@@ -0,0 +1,10 @@
+manual_cc_merchant_category_ref: dict[str, tuple[str, str]] = {
+    "Travel": ("Travel", "Travel"),
+}
+
+rogers_cc_merchant_category_ref: dict[str, tuple[str, str]] = {
+    "Eating Places and Restaurants": ("Food", "Eating Out"),
+    "Grocery Stores and Supermarkets": ("Food", "Grocery"),
+    "Miscellaneous Food Stores-Convenience Stores and Specialty Markets": ("Food", "Grocery"),
+    "Local and Suburban Commuter Passenger Transportation, including Ferries": ("Commuting", "Transit"),
+}
