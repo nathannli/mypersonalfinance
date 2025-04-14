716de1757557008ca227af97963ffdb7ee18058e
Nathan Li <nathan.li.1995@gmail.com>
2025-04-14 09:39:24 -0400

    (my finance db): incorporate cc category

--- classes/db/my_finance_db.py
+++ classes/db/my_finance_db.py
@@ -1,11 +1,18 @@
 from datetime import date
 import polars as pl
 from classes.db.generics.finance_db import FinanceDB
-
+from classes.cc.cc_merchant_category_ref import manual_cc_merchant_category_ref, rogers_cc_merchant_category_ref
 class MyFinanceDB(FinanceDB):
 
     def __init__(self, debug: bool = False):
         super().__init__(database_name="finance", debug=debug)
+
+    def get_subcategory_id_from_name(self, subcategory_name: str) -> int:
+        """
+        Get the subcategory id from the subcategory name.
+        """
+        query = "select id from subcategories where name = %s"
+        return self.select(query, (subcategory_name,))[0][0]
 
     def get_category_id_from_subcategory_id(self, subcategory_id: int) -> int:
         """
@@ -27,11 +34,24 @@ class MyFinanceDB(FinanceDB):
         }
         return pl.DataFrame(self.select(query), schema=schema)
 
-    def insert_expense(self, date: date, merchant: str, cost: float) -> None:
-
+    def insert_expense(self, date: date, merchant: str, cost: float, cc_category: str | None = None) -> None:
+        print("\n\n")
         print(f"Transaction on {date} at {merchant} for {cost}")
+        if cc_category is not None:
+            print(f"CC Category: {cc_category}")
+            ref_category_tuple = (manual_cc_merchant_category_ref.get(cc_category) or rogers_cc_merchant_category_ref.get(cc_category))
+            if ref_category_tuple is not None:
+                category, subcategory = ref_category_tuple
+                print(f"Ref Category: {category}, Ref Subcategory: {subcategory}")
+                subcategory_id = self.get_subcategory_id_from_name(subcategory)
+                category_id = self.get_category_id_from_subcategory_id(subcategory_id)
+                query = "insert into expenses (date, merchant, cost, category_id, subcategory_id) values (%s, %s, %s, %s, %s)"
+                self.insert(query, (date, merchant, cost, category_id, subcategory_id))
+                return
         df = self.get_subcategory_and_category()
+        print("\n\n")
         print(df)
+        print("\n\n")
         subcategory_id = input("Enter the subcategory id: ")
         category_id = self.get_category_id_from_subcategory_id(subcategory_id)
         query = "insert into expenses (date, merchant, cost, category_id, subcategory_id) values (%s, %s, %s, %s, %s)"
