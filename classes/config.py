ffe7b0dcc1ea376493c41188646064440061cbec
Nathan Li <nathan.li.1995@gmail.com>
2025-10-24 19:05:41 -0400

    typo in env var

--- classes/config.py
+++ classes/config.py
@@ -22,7 +22,7 @@ class Config:
         print(f"exists: {os.path.exists(dotenv_path)=}")
         load_dotenv(dotenv_path=dotenv_path)
         self.postgres_connection_string = os.getenv("POSTGRES_CONNECTION_STRING")
-        self.ws_debt_link = os.getenv("WS_DEBT_LINK")
+        self.ws_debt_link = os.getenv("WS_DEBIT_LINK")
         self.ws_credit_link = os.getenv("WS_CREDIT_LINK")
         self.debug = debug
         if self.debug:
